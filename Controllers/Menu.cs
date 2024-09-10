ff884d5462a5dbf12f309d81cd85bcac2246e404
darynazar <heydaro.std00@gmail.com>
2024-09-10 15:24:57 +0330

    add detail view

--- Controllers/Menu.cs
+++ Controllers/Menu.cs
@@ -17,5 +17,18 @@ namespace Hospital_Web.Controllers
         {
             return View( await _context.Dishes.ToListAsync());
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var dish = await _context.Dishes
+                .Include(di => di.DishIngredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+            return View(dish);
+        }
     }
 }
