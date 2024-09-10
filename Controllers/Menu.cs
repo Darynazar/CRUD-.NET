9576867688ecc1ae9ff65a786d4e0d390176d2dc
darynazar <heydaro.std00@gmail.com>
2024-09-10 11:19:53 +0330

    add the first view file to display the image, name and price

--- Controllers/Menu.cs
+++ Controllers/Menu.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Hospital_Web.Data;
+using Hospital_Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hospital_Web.Controllers
 {
     public class Menu : Controller
     {
-        public IActionResult Index()
+        private readonly MenuContext _context;
+        public Menu(MenuContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View( await _context.Dishes.ToListAsync());
         }
     }
 }
